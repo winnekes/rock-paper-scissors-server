63214b325d8aeef61c566383bb4ac0ef666fee5f
Simona Winnekes <simona.winnekes@googlemail.com>
2019-11-18 15:38:26 +0100

    Add endpoint for rooms with authentication

--- room/router.js
+++ room/router.js
@@ -8,14 +8,14 @@ const { Router } = express;
 function factory(stream) {
     const router = new Router();
 
-    router.post('/room', (request, response, next) => {
+    router.post('/room', auth, (request, response, next) => {
         Room.create(request.body).then(room => {
             const data = JSON.stringify(room);
             stream.send(data);
             response.send(room);
         });
     });
-    router.get('/rooms', (request, response, next) => {
+    router.get('/rooms', auth, (request, response, next) => {
         Room.findAll().then(rooms => {
             const data = JSON.stringify(rooms);
             stream.send(data);
