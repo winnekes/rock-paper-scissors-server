fe9bf6aea41a079fda9eb949887a7524316b614a
Simona Winnekes <simona.winnekes@googlemail.com>
2019-11-18 16:50:17 +0100

    Change auth settings

--- room/router.js
+++ room/router.js
@@ -8,14 +8,14 @@ const { Router } = express;
 function factory(stream) {
     const router = new Router();
 
-    router.post('/room', auth, (request, response, next) => {
+    router.post('/room', (request, response, next) => {
         Room.create(request.body).then(room => {
             const data = JSON.stringify(room);
             stream.send(data);
             response.send(room);
         });
     });
-    router.get('/rooms', auth, (request, response, next) => {
+    router.get('/rooms', (request, response, next) => {
         Room.findAll().then(rooms => {
             const data = JSON.stringify(rooms);
             stream.send(data);
