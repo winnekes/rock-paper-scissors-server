e85005ac53f56c48bfef25c0e64be80f376f45f9
Simona Winnekes <simona.winnekes@googlemail.com>
2019-11-19 10:33:22 +0100

    Add authentication post room route

--- room/router.js
+++ room/router.js
@@ -8,7 +8,7 @@ const { Router } = express;
 function factory(stream) {
     const router = new Router();
 
-    router.post('/room', (request, response, next) => {
+    router.post('/room', auth, (request, response, next) => {
         Room.create(request.body).then(room => {
             const data = JSON.stringify(room);
             stream.send(data);
